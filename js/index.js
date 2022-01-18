37edb9cae76f11dbc8d1653a54a52419ea23c32e
quin0304 <97483262+quin0304@users.noreply.github.com>
2022-01-18 16:32:17 -0500

    Created index.js

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+let a = 3;
+let b = 5;
+let c;
+let output = '';
+output += 'let a = 3\n';
+output += 'let b = 5\n';
+output += 'let c = undefined\n';
+output += '------------------\n';
+output += 'a + b = ' + (a + b) + '\n' + 'a - b = ' + (a - b) + '\n' + 'a * b = ' + (a * b) + '\n' + 'a / b = ' + (a / b) + '\n' + 'a % b = ' + (a % b) + '\n' + '(a += b) : ' + (a += b) + '\n' + '(a -= b) : ' + (a -= b) + '\n' + '(a *= b) : ' + (a *= b) + '\n' + '(a /= b) : ' + (a /= b) + '\n' + '(a %= b) : ' + (a %= b) + '\n' + '(a == b) : ' + (a == b) + '\n' + '(a != b) : ' + (a != b) + '\n' + '(a > b) : ' + (a > b) + '\n' + '(a < b) : ' + (a < b) + '\n' + '(!a && !c) : ' + (!a && !c) + '\n' + '(!a || !c) : ' + (!a || !c) + '\n' ;
+
+
+
+let first_name = 'Alexei';
+let last_name = 'Quintero';
+let email = 'quin0304@algonquinlive.com';
+
+output += '-------- Task 2 ----------\n';
+output += 'My name is ' + (first_name) + ' ' + (last_name) + '. You can contact me at ' + (email) + '.' ;
+
+alert (output);
